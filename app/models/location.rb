399a294a4b478df1b9c7e54792136f5ca3e8ac76
Yrvin Escorihuela <yher136@gmail.com>
2017-04-02 16:09:57 -0300

    OpenWeatherAPI Ready

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,20 +3,33 @@ class Location < ApplicationRecord
   has_many :temperature
 
   def self.get_country_location(location_id)
-  	data_current_country = self.get_current_country(location_id)
+    data_current_country = self.get_current_country(location_id)
+    if data_current_country.empty? 
+      return Hash['message' => 'No encontrado', 'code' => '404']
+    end
     if $redis.hgetall("#{data_current_country[0][2]}").empty? then
-	    data_owa = Temperature.data_caching_temperature(data_current_country[0][1], data_current_country[0][2])
-	    data_json_owa = JSON.parse(data_owa)
-	    $redis.hmset("#{data_current_country[0][2]}", "current_temp_kelvin", data_json_owa["main"]["temp"],\
-	    											  "current_temp_celsius", (data_json_owa["main"]["temp"] - 273).round(2),\
-	    											  "current_temp_farenheit", ((((data_json_owa["main"]["temp"] - 273) + 32) * 9) / 5).round(2),\
-	    											  "location_id", data_current_country[0][3])	    
-	end
-	$redis.hgetall("#{data_current_country[0][2]}")
+      data_owa = Temperature.data_caching_temperature(data_current_country[0][1], data_current_country[0][2])
+      data_json_owa = JSON.parse(data_owa)
+      $redis.hmset("#{data_current_country[0][2]}", "current_temp_kelvin", data_json_owa["main"]["temp"],\
+        "current_temp_celsius", (data_json_owa["main"]["temp"] - 273).round(2),\
+        "current_temp_farenheit", ((((data_json_owa["main"]["temp"] - 273) + 32) * 9) / 5).round(2),\
+        "location_id", data_current_country[0][3],
+        "dt_source", data_json_owa["dt"], 'code', 200)
+      
+      data = Array[data_json_owa["main"]["temp"], data_current_country[0][3], data_json_owa["dt"]]
+      self.add_new_data(data)
+  end
+  $redis.hgetall("#{data_current_country[0][2]}")
   end
   private
   def self.get_current_country(location_id)
-  	current_country = self.select(:name_country, :initials, :name_location, :id).joins(:country).where(["locations.id = %d", "#{location_id}"]).limit(1).pluck(:name_country, :initials, :name_location, :id)
-  	return current_country
+    current_country = self.select(:name_country, :initials, :name_location, :id, :country_id)\
+    .joins(:country).where(["locations.id = %d", "#{location_id}"]).limit(1)\
+    .pluck(:name_country, :initials, :name_location, :id, :country_id)
+    return current_country
+  end
+
+  def self.add_new_data(data)
+    Temperature.store_temp_data(data)
   end
 end
--- app/models/temperature.rb
+++ app/models/temperature.rb
@@ -2,27 +2,15 @@ class Temperature < ApplicationRecord
   belongs_to :location
 
   validates :temperature, presence: true
+  validates :location_id, presence: true
   validates :generated_at, presence: true
 
-  def self.get_temp(location_id)
-    query_temp = "select temperature as temp_kelvin, (temperature - 273) as temp_celsius, \
-    ((((temperature - 273) + 32) * 9) / 5)::decimal(11,2) as temp_farenheit,\
-    max(to_timestamp(generated_at)) as api_dt  \
-    from temperatures where location_id = #{location_id} group by temperature limit 1"
-    
-    self.find_by_sql query_temp
-  end
-
-
   def self.data_caching_temperature(location, country_initials)
-    data = RestClient::Request.execute(method: :get, url: 'http://api.openweathermap.org/data/2.5/weather', timeout: 10, headers: {params: {q:"#{location}, #{country_initials}" , appid: 'bdcfb8b99a0839613f4ce6d6df6a1c9b'}})   
-    
+    data = RestClient::Request.execute(method: :get, url: 'http://api.openweathermap.org/data/2.5/weather', timeout: 10, headers: {params: {q:"#{location}, #{country_initials}" , appid: 'bdcfb8b99a0839613f4ce6d6df6a1c9b'}})
   end
-  
 
-
-
-
-
-
+  def self.store_temp_data(data)    
+    self.create(temperature: data[0], location_id: data[1], generated_at: data[2])
+  end
+ 
 end
